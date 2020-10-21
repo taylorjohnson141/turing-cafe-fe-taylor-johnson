d132f6a12beb1c9618d7138edf964361e9d68637
Taylor Johnson <taylorjohnson141@gmail.com>
2020-10-21 09:55:13 -0600

    Add basic Reservation Form

--- src/ReservationForm.js
+++ src/ReservationForm.js
@@ -8,4 +8,23 @@ class ReservationForm extends Component{
 
   }
 }
+render(){
+  return (
+    <form >
+      <label for="Name">
+      <input type="text"/>
+      </label>
+      <label for="date">
+        <input type="text"/>
+      </label>
+      <label for="time">
+      <input type="text"/>
+      </label>
+      <label for="numberOfGuests">
+        <input type="text"/>
+      </label>
+      <input type="submit" value="Make Reservation" onSubmit = {} />
+    </form>
+  )
+}
 }
