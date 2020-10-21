669d38204177ada98f67c7b36fc5085f628473e0
Taylor Johnson <taylorjohnson141@gmail.com>
2020-10-21 11:47:32 -0600

    Add initial Reservation form test

--- src/ReservationFormtest.js
+++ src/ReservationFormtest.js
@@ -1,2 +1,11 @@
 import React from 'react'
-import {screen} from '@testing-library/react'
+import {render, screen} from '@testing-library/react'
+import ReservationForm from './ReservationForm'
+import '@testing-library/jest-dom'
+describe('ReservationForm', () =>{
+  it('should render a form ', () =>{
+    render(<ReservationForm reservations ={''} />)
+    let form = screen.getAllByText(' Name:')
+    expect(form).toBeInTheDocument()
+  })
+})
