3b8419e76a2e0dda713b5058032441c27497e942
Taylor Johnson <taylorjohnson141@gmail.com>
2020-10-21 09:36:28 -0600

    Finish reservations component

--- src/Reservations.js
+++ src/Reservations.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {getReservations} from './apiCalls'
 class Reservations extends Component{
   constructor(){
     super()
@@ -7,5 +8,26 @@ class Reservations extends Component{
 
   }
 }
-
+async componentDidMount(){
+ let reservations = await getReservations()
+ this.setState({reservations:reservations})
+}
+formatReservations(){
+  return this.state.reservations.map(reservation =>{
+    <section id = {reservation.id}>
+      <h1>{reservation.name}</h1>
+  <h2>{reservation.date}</h2>
+          <h3>{reservation.time}</h3>
+  <h4>{reservation.number}</h4>
+      </section>
+  })
+}
+render(){
+  return(
+  <article>
+     (!this.state && <article>'Loading'</article>)
+    {this.formatReservations}
+  </article>
+  )
+}
 }
